b3ee59e8fbc73546c72c38c0e45aadc589d96f2e
ggabdoll <ggabdoll0327@naver.com>
2021-11-12 18:05:03 +0900

    11.12 lotto

---
+++ web/js/lotto.js
@@ -0,0 +1,34 @@
+
+
+const btn = document.getElementById("btn")
+
+console.log(btn)
+
+btn.addEventListener('click', (e) => {
+    const target = document.getElementById("resultDiv")
+    const balls = getLottoNum()
+
+    target.innerHTML = '';
+
+    for (const number of balls) {
+    const tag = document.createElement("h3");
+   const textNode = document.createTextNode(number)
+        tag.appendChild(textNode)
+    target.appendChild(tag)
+    }
+}, false)
+
+function getLottoNum() {
+let numArr = []
+    while (true) {
+        let randomNum = parseInt(Math.random() * 45) + 1;
+
+        if(numArr.indexOf(randomNum) == -1){
+            numArr.push(randomNum)
+        }
+        if (numArr.length == 6){
+            break
+        }
+    }
+    return numArr
+}
