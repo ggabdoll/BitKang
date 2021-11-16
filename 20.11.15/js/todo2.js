57f032d1d2eef702d49064222f97db3942ef8bef
ggabdoll <ggabdoll0327@naver.com>
2021-11-16 18:55:54 +0900

    practice

--- 20.11.15/js/todo2.js
+++ 20.11.15/js/todo2.js
@@ -1,15 +1,23 @@
 function outer() {
 
-    const arr = []
+    let arr = []
 let idx = 0
 
     function add(todo) {
         todo.idx = idx++
         arr.push(todo)
+
+        const localStorageStr = JSON.stringify(arr)
+
+        localStorage.setItem("todoList", localStorageStr)
     }
 
     function removeTodo(num){
         console.log("remove Todo....")
+
+       const result = arr.filter(todo => todo.idx !== num)
+
+        arr = result
     }
 
     function getTodo(idx){
@@ -17,10 +25,18 @@ let idx = 0
     }
 
     function getAll(){
-        return arr
-    }
 
-    const obj = {add, removeTodo, getTodo, getAll}
+        const localStorageList = JSON.parse(localStorage.getItem("todoList"))
+        console.log(localStorageList)
+
+        return localStorageList
+    }
+    function  changeAll(){
+        for (let i = 0; i < arr.length; i++) {
+            arr[i].complete = !arr[i].complete
+        }
+    }
+    const obj = {add, removeTodo, getTodo, getAll, changeAll}
 
     return obj
 
