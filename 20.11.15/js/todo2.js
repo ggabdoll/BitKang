dcd89af2ebeb124d32dbd59c5059f67b1f4e977b
ggabdoll <ggabdoll0327@naver.com>
2021-11-17 14:20:15 +0900

    morning lecture contents

--- 20.11.15/js/todo2.js
+++ 20.11.15/js/todo2.js
@@ -48,3 +48,35 @@ localStorage.setItem("todoList",JSON.stringify(localStorageList))
     return obj
 
 }
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('.menuDiv');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('.menuDiv');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '.menuDiv',
+            offset: 72,
+        });
+    };
+});
